3f776ed8b1ae71dae7cc42a1ba5f82149f330a64
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-04-27 09:30:58 +0200

    Add optional rate limit headers
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

---
+++ src/key_extractor.rs
@@ -0,0 +1,82 @@
+use std::{fmt::Display, hash::Hash, net::IpAddr};
+
+use actix_web::dev::ServiceRequest;
+
+/// Generic structure of what is needed to extract a rate-limiting key from an incoming request.
+pub trait KeyExtractor: Clone + Copy {
+    /// The type of the key.
+    type Key: Clone + Hash + Eq;
+
+    /// The type of the error that can occur if key extraction from the request fails.
+    type KeyExtractionError: Display;
+
+    #[cfg(feature = "log")]
+    /// Name of this extractor (only used in logs).
+    fn name(&self) -> &'static str;
+
+    /// Extraction method
+    fn extract(&self, req: &ServiceRequest) -> Result<Self::Key, Self::KeyExtractionError>;
+
+    #[cfg(feature = "log")]
+    /// Value of the extracted key (only used in logs).
+    fn key_name(&self, _key: &Self::Key) -> Option<String> {
+        None
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+/// A [KeyExtractor] that uses peer IP as key. **This is the default key extractor and [it may no do want you want](PeerIpKeyExtractor).**
+///
+/// **Warning:** this key extractor enforces rate limiting based on the **_peer_ IP address**.
+///
+/// This means that if your app is deployed behind a reverse proxy, the peer IP address will _always_ be the proxy's IP address.
+/// In this case, rate limiting will be applied to _all_ incoming requests as if they were from the same user.
+///
+/// If this is not the behavior you want, you may:
+/// - implement your own [KeyExtractor] that tries to get IP from the `Forwarded` or `X-Forwarded-For` headers that most reverse proxies set
+/// - make absolutely sure that you only trust these headers when the peer IP is the IP of your reverse proxy (otherwise any user could set them to fake its IP)
+pub struct PeerIpKeyExtractor;
+
+impl KeyExtractor for PeerIpKeyExtractor {
+    type Key = IpAddr;
+    type KeyExtractionError = &'static str;
+
+    #[cfg(feature = "log")]
+    fn name(&self) -> &'static str {
+        "peer IP"
+    }
+
+    fn extract(&self, req: &ServiceRequest) -> Result<Self::Key, Self::KeyExtractionError> {
+        req.peer_addr()
+            .map(|socket| socket.ip())
+            .ok_or("Could not extract peer IP address from request")
+    }
+
+    #[cfg(feature = "log")]
+    fn key_name(&self, key: &Self::Key) -> Option<String> {
+        Some(key.to_string())
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+/// A [KeyExtractor] that allow to do rate limiting for all incoming requests. This is useful if you want to hard-limit the HTTP load your app can handle.
+pub struct GlobalKeyExtractor;
+
+impl KeyExtractor for GlobalKeyExtractor {
+    type Key = ();
+    type KeyExtractionError = &'static str;
+
+    #[cfg(feature = "log")]
+    fn name(&self) -> &'static str {
+        "global"
+    }
+
+    fn extract(&self, _req: &ServiceRequest) -> Result<Self::Key, Self::KeyExtractionError> {
+        Ok(())
+    }
+
+    #[cfg(feature = "log")]
+    fn key_name(&self, _key: &Self::Key) -> Option<String> {
+        None
+    }
+}
---
+++ src/service.rs
@@ -0,0 +1,235 @@
+use actix_web::dev::{Service, ServiceRequest, ServiceResponse};
+use actix_web::http::header::{HeaderName, HeaderValue};
+use actix_web::{body::MessageBody, error, Error};
+use futures::future;
+use governor::clock::{Clock, DefaultClock};
+use governor::middleware::{NoOpMiddleware, StateInformationMiddleware};
+
+use std::future::Future;
+use std::marker::Unpin;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use crate::{GovernorMiddleware, KeyExtractor};
+
+impl<S, B, K> Service<ServiceRequest> for GovernorMiddleware<S, K, NoOpMiddleware>
+where
+    K: KeyExtractor,
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    B: MessageBody,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future =
+        future::Either<future::Ready<Result<ServiceResponse<B>, actix_web::Error>>, S::Future>;
+
+    fn poll_ready(&self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        if let Some(configured_methods) = &self.methods {
+            if !configured_methods.contains(req.method()) {
+                // The request method is not configured, we're ignoring this one.
+                let fut = self.service.call(req);
+                return future::Either::Right(fut);
+            }
+        }
+
+        // Use the provided key extractor to extract the rate limiting key from the request.
+        match self.key_extractor.extract(&req) {
+            // Extraction worked, let's check if rate limiting is needed.
+            Ok(key) => match self.limiter.check_key(&key) {
+                Ok(_) => {
+                    let fut = self.service.call(req);
+                    future::Either::Right(fut)
+                }
+
+                Err(negative) => {
+                    let wait_time = negative
+                        .wait_time_from(DefaultClock::default().now())
+                        .as_secs();
+
+                    #[cfg(feature = "log")]
+                    {
+                        let key_name = match self.key_extractor.key_name(&key) {
+                            Some(n) => format!(" [{}]", &n),
+                            None => "".to_owned(),
+                        };
+                        log::info!(
+                            "Rate limit exceeded for {}{}, quota reset in {}s",
+                            self.key_extractor.name(),
+                            key_name,
+                            &wait_time
+                        );
+                    }
+
+                    let wait_time_str = wait_time.to_string();
+                    let body = format!("Too many requests, retry in {}s", wait_time_str);
+                    let response = actix_web::HttpResponse::TooManyRequests()
+                        .insert_header((actix_web::http::header::RETRY_AFTER, wait_time_str))
+                        .body(body.clone());
+                    future::Either::Left(future::err(
+                        error::InternalError::from_response(body, response).into(),
+                    ))
+                }
+            },
+
+            // Extraction failed, stop right now with a HTTP 500 error.
+            Err(e) => {
+                future::Either::Left(future::err(error::ErrorInternalServerError(e.to_string())))
+            }
+        }
+    }
+}
+
+pub struct RateLimitHeaderFut<F>
+where
+    F: Future,
+{
+    future: F,
+    burst_size: u32,
+    remaining_burst_capacity: u32,
+}
+
+impl<F, B> Future for RateLimitHeaderFut<F>
+where
+    F: Future<Output = Result<ServiceResponse<B>, actix_web::Error>> + Unpin,
+    B: MessageBody,
+{
+    type Output = F::Output;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match Future::poll(Pin::new(&mut self.future), cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(response) => Poll::Ready(match response {
+                Ok(mut response) => {
+                    let headers = response.headers_mut();
+                    headers.insert(
+                        HeaderName::from_static("x-ratelimit-limit"),
+                        self.burst_size.into(),
+                    );
+                    headers.insert(
+                        HeaderName::from_static("x-ratelimit-remaining"),
+                        self.remaining_burst_capacity.into(),
+                    );
+                    Ok(response)
+                }
+                Err(err) => Err(err),
+            }),
+        }
+    }
+}
+
+pub struct WhitelistedHeaderFut<F>
+where
+    F: Future,
+{
+    future: F,
+}
+
+impl<F, B> Future for WhitelistedHeaderFut<F>
+where
+    F: Future<Output = Result<ServiceResponse<B>, actix_web::Error>> + Unpin,
+    B: MessageBody,
+{
+    type Output = F::Output;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match Future::poll(Pin::new(&mut self.future), cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(response) => Poll::Ready(match response {
+                Ok(mut response) => {
+                    let headers = response.headers_mut();
+                    headers.insert(
+                        HeaderName::from_static("x-ratelimit-whitelisted"),
+                        HeaderValue::from_static("true"),
+                    );
+                    Ok(response)
+                }
+                Err(err) => Err(err),
+            }),
+        }
+    }
+}
+
+/// Implementation using rate limit headers
+impl<S, B, K> Service<ServiceRequest> for GovernorMiddleware<S, K, StateInformationMiddleware>
+where
+    K: KeyExtractor,
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    B: MessageBody,
+    S::Future: Unpin,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = future::Either<
+        future::Ready<Result<ServiceResponse<B>, actix_web::Error>>,
+        future::Either<RateLimitHeaderFut<S::Future>, WhitelistedHeaderFut<S::Future>>,
+    >;
+
+    fn poll_ready(&self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        if let Some(configured_methods) = &self.methods {
+            if !configured_methods.contains(req.method()) {
+                // The request method is not configured, we're ignoring this one.
+                let fut = self.service.call(req);
+                return future::Either::Right(future::Either::Right(WhitelistedHeaderFut {
+                    future: fut,
+                }));
+            }
+        }
+
+        // Use the provided key extractor to extract the rate limiting key from the request.
+        match self.key_extractor.extract(&req) {
+            // Extraction worked, let's check if rate limiting is needed.
+            Ok(key) => match self.limiter.check_key(&key) {
+                Ok(snapshot) => {
+                    let fut = self.service.call(req);
+                    future::Either::Right(future::Either::Left(RateLimitHeaderFut {
+                        future: fut,
+                        burst_size: snapshot.quota().burst_size().get(),
+                        remaining_burst_capacity: snapshot.remaining_burst_capacity(),
+                    }))
+                }
+
+                Err(negative) => {
+                    let wait_time = negative
+                        .wait_time_from(DefaultClock::default().now())
+                        .as_secs();
+
+                    #[cfg(feature = "log")]
+                    {
+                        let key_name = match self.key_extractor.key_name(&key) {
+                            Some(n) => format!(" [{}]", &n),
+                            None => "".to_owned(),
+                        };
+                        log::info!(
+                            "Rate limit exceeded for {}{}, quota reset in {}s",
+                            self.key_extractor.name(),
+                            key_name,
+                            &wait_time
+                        );
+                    }
+
+                    let wait_time_str = wait_time.to_string();
+                    let body = format!("Too many requests, retry in {}s", wait_time_str);
+                    let response = actix_web::HttpResponse::TooManyRequests()
+                        .insert_header((actix_web::http::header::RETRY_AFTER, wait_time_str))
+                        .body(body.clone());
+                    future::Either::Left(future::err(
+                        error::InternalError::from_response(body, response).into(),
+                    ))
+                }
+            },
+
+            // Extraction failed, stop right now with a HTTP 500 error.
+            Err(e) => {
+                future::Either::Left(future::err(error::ErrorInternalServerError(e.to_string())))
+            }
+        }
+    }
+}
