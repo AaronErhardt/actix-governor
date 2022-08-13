27cf9aac690c2d4b63a2e82ad3df4fc7e6ef2035
TheAwiteb <awiteb@hotmail.com>
2022-08-13 15:16:00 +0300

    ✨: Add the trait `ResponseError` to `KeyExtractor::KeyExtractionError` type and remove `response_error` function from `KeyExtractor` [BC]

--- src/key_extractor.rs
+++ src/key_extractor.rs
@@ -1,8 +1,10 @@
+use actix_web::ResponseError;
 use actix_web::{dev::ServiceRequest, http::header::ContentType};
 use governor::clock::{Clock, DefaultClock, QuantaInstant};
 use governor::NotUntil;
 
-use std::{fmt::Display, hash::Hash, net::IpAddr};
+use std::fmt::{Debug, Display};
+use std::{hash::Hash, net::IpAddr};
 
 /// Generic structure of what is needed to extract a rate-limiting key from an incoming request.
 pub trait KeyExtractor: Clone {
@@ -10,13 +12,15 @@ pub trait KeyExtractor: Clone {
     type Key: Clone + Hash + Eq;
 
     /// The type of the error that can occur if key extraction from the request fails.
-    type KeyExtractionError: Display;
+    type KeyExtractionError: ResponseError + 'static;
 
     #[cfg(feature = "log")]
     /// Name of this extractor (only used in logs).
     fn name(&self) -> &'static str;
 
-    /// Extraction method
+    /// Extraction method, will return [`KeyExtractionError`] response when the extract failed
+    ///
+    /// [`KeyExtractionError`]: KeyExtractor::KeyExtractionError
     fn extract(&self, req: &ServiceRequest) -> Result<Self::Key, Self::KeyExtractionError>;
 
     /// The content you want to show it when the rate limit is exceeded.
@@ -30,14 +34,6 @@ pub trait KeyExtractor: Clone {
             format!("Too many requests, retry in {}s", wait_time),
             ContentType::plaintext(),
         )
-    }
-
-    /// Error function, will pass [`Self::KeyExtractionError`] to it to return the response error
-    /// when the [`Self::extract`] failed [Read more]
-    ///
-    /// [Read more]: https://docs.rs/actix-web/4.1.0/actix_web/error/index.html#functions
-    fn response_error(&self, err: Self::KeyExtractionError) -> actix_web::Error {
-        actix_web::error::ErrorInternalServerError(err.to_string())
     }
 
     #[cfg(feature = "log")]
@@ -60,9 +56,21 @@ pub trait KeyExtractor: Clone {
 /// - make absolutely sure that you only trust these headers when the peer IP is the IP of your reverse proxy (otherwise any user could set them to fake its IP)
 pub struct PeerIpKeyExtractor;
 
+#[derive(Debug)]
+/// The error of PeerIpKeyExtractor
+pub struct PeerIpKeyExtractionError;
+
+impl Display for PeerIpKeyExtractionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Could not extract peer IP address from request")
+    }
+}
+
+impl ResponseError for PeerIpKeyExtractionError {}
+
 impl KeyExtractor for PeerIpKeyExtractor {
     type Key = IpAddr;
-    type KeyExtractionError = &'static str;
+    type KeyExtractionError = PeerIpKeyExtractionError;
 
     #[cfg(feature = "log")]
     fn name(&self) -> &'static str {
@@ -72,7 +80,7 @@ impl KeyExtractor for PeerIpKeyExtractor {
     fn extract(&self, req: &ServiceRequest) -> Result<Self::Key, Self::KeyExtractionError> {
         req.peer_addr()
             .map(|socket| socket.ip())
-            .ok_or("Could not extract peer IP address from request")
+            .ok_or(Self::KeyExtractionError {})
     }
 
     #[cfg(feature = "log")]
@@ -85,9 +93,21 @@ impl KeyExtractor for PeerIpKeyExtractor {
 /// A [KeyExtractor] that allow to do rate limiting for all incoming requests. This is useful if you want to hard-limit the HTTP load your app can handle.
 pub struct GlobalKeyExtractor;
 
+#[derive(Debug)]
+/// A [KeyExtractor] default error, with 500 server error and plintext response ( a content is .0 )
+pub struct GlobalKeyExtractionError<T: Display + Debug>(pub T);
+
+impl<T: Display + Debug> Display for GlobalKeyExtractionError<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl<T: Display + Debug> ResponseError for GlobalKeyExtractionError<T> {}
+
 impl KeyExtractor for GlobalKeyExtractor {
     type Key = ();
-    type KeyExtractionError = &'static str;
+    type KeyExtractionError = GlobalKeyExtractionError<&'static str>;
 
     #[cfg(feature = "log")]
     fn name(&self) -> &'static str {
